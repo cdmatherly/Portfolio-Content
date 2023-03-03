829009d8e0e53169790b9592634feb8ed720149f
Chase <chsmatherly@gmail.com>
2023-03-02 17:17:32 -0800

    folded in navbar function, added project cards

--- script.js
+++ script.js
@@ -1,41 +1,133 @@
 var link = document.querySelectorAll('.nav-link')
-var sec = document.querySelectorAll('.nav-sec')
+var sect = document.querySelectorAll('.nav-sec')
 // Define the scroll threshold in pixels
-var scrollThreshold = [sec[0].offsetTop, sec[1].offsetTop, sec[2].offsetTop, sec[3].offsetTop];
-console.log(sec[2].offsetTop)
-console.log(sec)
-// Add an event listener to the window object to detect scroll
-window.addEventListener('scroll', () => {
-  // Get the current scroll position
-  var scrollPosition = window.scrollY;
+var homeTarget = document.querySelector("#home");
+var scrollThreshold = [sect[0].offsetTop, sect[1].offsetTop, sect[2].offsetTop, sect[3].offsetTop];
+// console.log(sect[2].offsetTop)
+(function() {
+  getProjects();
+})()
 
-  // Check if the scroll position has passed the threshold
-  if (scrollPosition > 0 && scrollPosition < scrollThreshold[1]) {
-    // Add the class to the element
-    link[0].classList.add('active');
-    link[1].classList.remove('active')
-    link[2].classList.remove('active')
-    link[3].classList.remove('active');
-  } 
-  else if (scrollPosition > scrollThreshold[1] && scrollPosition < scrollThreshold[2]) {
-    // Add the class to the element
-    link[0].classList.remove('active')
-    link[2].classList.remove('active')
-    link[3].classList.remove('active');
-    link[1].classList.add('active');
-  } 
-  else if (scrollPosition > scrollThreshold[2] && scrollPosition < scrollThreshold[3]) {
-    // Add the class to the element
-    link[0].classList.remove('active')
-    link[1].classList.remove('active')
-    link[3].classList.remove('active');
-    link[2].classList.add('active');
-  } 
-  else if (scrollPosition > scrollThreshold[3]) {
-    // Add the class to the element
-    link[0].classList.remove('active')
-    link[1].classList.remove('active')
-    link[2].classList.remove('active');
-    link[3].classList.add('active');
+// Add an event listener to the window object to detect scroll
+
+function removeClass(){
+  for (var i = 0; i < link.length; i++){
+    link[i].classList.remove('active');
+  };
+};
+function testFunc(){
+  var scrollPosition = window.scrollY;
+  for (var i = 0; i < scrollThreshold.length; i++){
+    if (scrollPosition > sect[i].offsetTop){
+      removeClass();
+      link[i].classList.add('active');
+    }
   }
-});
+}
+
+window.addEventListener('scroll', testFunc);
+
+var projects = document.querySelector('.projectList')
+
+function getProjects(){
+$.getJSON('https://api.github.com/users/cdmatherly/repos?per_page=53', (data) => {
+  data.forEach((element) => {
+      if (element.fork == false) {
+          $.getJSON(element.languages_url, (data) => {
+              populate(element.name, element.pushed_at, Object.keys(data), element);
+          })
+      }
+  })
+})
+}
+
+function populate(name, updated, languages, element) {
+  var card = document.createElement("div");
+  card.classList.add("card");
+
+  addLanguages(languages, card);
+
+  var title = document.createElement("a");
+  title.classList.add("title");
+  var name_parts = name.replace("-", " ");
+  title.innerHTML = name_parts;
+  title.setAttribute("href", element.html_url);
+  card.appendChild(title);
+
+  var date = updated.split("T")[0]
+  var date_0 = date.split("-");
+
+  var year = date_0[0];
+  var month = date_0[1];
+  var day = date_0[2];
+
+  var date = document.createElement("div");
+  date.classList.add("date");
+
+  var subtitle = document.createElement("code");
+  subtitle.classList.add("last-updated");
+  subtitle.classList.add("mb-0");
+  subtitle.innerHTML = "Last updated on: </br>"
+  date.appendChild(subtitle);
+
+  var last_updated = document.createElement("code");
+  last_updated.innerHTML = month + "/" + day + "/" + year;
+  last_updated.classList.add("last-updated");
+  last_updated.classList.add("mt-0");
+  date.appendChild(last_updated);
+
+  card.appendChild(date);
+
+  projects.appendChild(card);
+}
+
+function addLanguages(languages, card) {
+  var lang_icons = document.createElement("div");
+  lang_icons.classList.add("d-flex");
+  lang_icons.classList.add("languages_list");
+
+  var icon = document.createElement("i");
+  icon.classList.add("bx");
+  if (languages.length == 0) {
+      icon.innerHTML = " ";
+  }
+  if (languages.includes("HTML")) {
+      var icon2 = document.createElement("i");
+      icon2.classList.add("bx");
+      icon2.classList.add("bxl-html5");
+      icon2.classList.add("html");
+      lang_icons.appendChild(icon2);
+  }
+  if (languages.includes("Python")) {
+      var icon3 = document.createElement("i");
+      icon3.classList.add("bx");
+      icon3.classList.add("bxl-python");
+      icon3.classList.add("python");
+      lang_icons.appendChild(icon3);
+  }
+  if (languages.includes("JavaScript")) {
+      var icon4 = document.createElement("i");
+      icon4.classList.add("bx");
+      icon4.classList.add("bxl-javascript");
+      icon4.classList.add("javascript");
+      lang_icons.appendChild(icon4);
+  }
+  if (languages.includes("CSS")) {
+      var icon5 = document.createElement("i");
+      icon5.classList.add("bx");
+      icon5.classList.add("bxl-css3");
+      icon5.classList.add("css");
+      lang_icons.appendChild(icon5);
+  }
+  if (languages.includes("Java")) {
+      var icon9 = document.createElement("i");
+      icon9.classList.add("bx");
+      icon9.classList.add("bxl-java");
+      icon9.classList.add("plain-java");
+      lang_icons.appendChild(icon9);
+  }
+  lang_icons.appendChild(icon);
+
+
+  card.appendChild(lang_icons);
+}
